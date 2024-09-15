ac1d08bddb16c9befe47b8b9625a2caf3dfa6305
Jim Lightfoot <jim.lightfoot@outlook.com>
2024-09-15 11:43:36 -0700

    Handle error codes

--- Deployment/CustomTasks/JTranTransform/AzureDevOps/Deploy/buildandreleasetask/Transform.ps1
+++ Deployment/CustomTasks/JTranTransform/AzureDevOps/Deploy/buildandreleasetask/Transform.ps1
@@ -17,25 +17,25 @@ dotnet tool install jtrancmd -g
 
 echo '....'
 
-[string]$InputSourcePath       = Get-VstsInput -Name InputSourcePath
-[string]$TransformPath         = Get-VstsInput -Name TransformPath
-[string]$OutputDestinationPath = Get-VstsInput -Name OutputDestinationPath
-[string]$IncludePath           = Get-VstsInput -Name IncludePath  
-[string]$DocumentPath          = Get-VstsInput -Name DocumentPath
-[bool]$MultipleOutput          = Get-VstsInput -Name MultipleOutput -AsBool
-[string]$TransformParameters   = Get-VstsInput -Name TransformParameters 
-[string]$ArgumentsProviderPath = Get-VstsInput -Name ArgumentsProviderPath 
-[string]$ArgumentsClass        = Get-VstsInput -Name ArgumentsClass 
+#[string]$InputSourcePath       = Get-VstsInput -Name InputSourcePath
+#[string]$TransformPath         = Get-VstsInput -Name TransformPath
+#[string]$OutputDestinationPath = Get-VstsInput -Name OutputDestinationPath
+#[string]$IncludePath           = Get-VstsInput -Name IncludePath  
+#[string]$DocumentPath          = Get-VstsInput -Name DocumentPath
+#[bool]$MultipleOutput          = Get-VstsInput -Name MultipleOutput -AsBool
+#[string]$TransformParameters   = Get-VstsInput -Name TransformParameters 
+#[string]$ArgumentsProviderPath = Get-VstsInput -Name ArgumentsProviderPath 
+#[string]$ArgumentsClass        = Get-VstsInput -Name ArgumentsClass 
 
-#[string]$InputSourcePath       = $args[0]
-#[string]$TransformPath         = $args[1]
-#[string]$OutputDestinationPath = $args[2]
-#[string]$IncludePath           = $args[3]
-#[string]$DocumentPath          = $args[4]
-#[bool]$MultipleOutput          = $args[5]
-#[string]$TransformParameters   = $args[6] 
-#[string]$ArgumentsPath         = $args[7] 
-#[string]$ArgumentsClass        = $args[8] 
+[string]$InputSourcePath       = $args[0]
+[string]$TransformPath         = $args[1]
+[string]$OutputDestinationPath = $args[2]
+[string]$IncludePath           = $args[3]
+[string]$DocumentPath          = $args[4]
+[bool]$MultipleOutput          = $args[5]
+[string]$TransformParameters   = $args[6] 
+[string]$ArgumentsPath         = $args[7] 
+[string]$ArgumentsClass        = $args[8] 
 
 [string]$sMultipleOutput       = if($MultipleOutput) {'true'} else {'false'}
 [string]$ArgumentsProvider     = if($ArgumentsProviderPath -ne '') {$ArgumentsProviderPath + '::' + $ArgumentsClass} else {''} 
@@ -52,6 +52,14 @@ echo 'Transforming input file(s)...'
 Invoke-Expression $CommandLine
 
 echo '....'
-echo '...Successful'
+
+if($LASTEXITCODE -eq 0)
+{
+  echo '...Transform succeeded'
+}
+else
+{
+  Write-Error "Transform failed"
+}
 
 ## .\Transform.ps1 C:\Development\Projects\JTranOrg\JTran.CICD\Deployment\CustomTasks\JTranTransform\AzureDevOps\Test\blanksource.json C:\Development\Projects\JTranOrg\JTran.CICD\Deployment\CustomTasks\JTranTransform\AzureDevOps\Test\config.jtran C:\Development\Testing\JTran\JTranTransform\output.json C:\Development\Testing\JTran\JTranTransform C:\Development\Testing\JTran\JTranTransform false "-environment 'prod'" "C:\Development\Projects\JTranOrg\JTran\Tests\TestArgumentsProvider\bin\Debug\net8.0\TestArgumentsProvider.dll" TestArgumentsProvider.MyArgs
