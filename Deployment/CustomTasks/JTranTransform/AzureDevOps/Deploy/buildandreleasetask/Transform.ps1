907d93332d365cbcdd6a22e204ab455334a2018a
Jim Lightfoot <jim.lightfoot@outlook.com>
2024-09-15 13:01:31 -0700

    Updates

--- Deployment/CustomTasks/JTranTransform/AzureDevOps/Deploy/buildandreleasetask/Transform.ps1
+++ Deployment/CustomTasks/JTranTransform/AzureDevOps/Deploy/buildandreleasetask/Transform.ps1
@@ -1,13 +1,14 @@
 
-echo '##############################################################'
-echo '### JTranTransform'
-echo '###'
-echo '###    Transforms a jtran transform source into a json destination'
-echo '###      (or multiple destinations)'
-echo '###'
-echo '### Copyright (c) June 2024 - Jim Lightfoot - All Rights Reserved'
-echo '###'
-echo '##############################################################'
+##############################################################
+### JTranTransform
+###
+###    Transforms a jtran transform source into a json destination
+###      (or multiple destinations)
+###
+### Copyright (c) 2024 - Jim Lightfoot - All Rights Reserved
+###
+##############################################################
+
 echo '....'
 echo 'Starting JTran transformation...'
 echo '....'
@@ -17,25 +18,25 @@ dotnet tool install jtrancmd -g
 
 echo '....'
 
-#[string]$InputSourcePath       = Get-VstsInput -Name InputSourcePath
-#[string]$TransformPath         = Get-VstsInput -Name TransformPath
-#[string]$OutputDestinationPath = Get-VstsInput -Name OutputDestinationPath
-#[string]$IncludePath           = Get-VstsInput -Name IncludePath  
-#[string]$DocumentPath          = Get-VstsInput -Name DocumentPath
-#[bool]$MultipleOutput          = Get-VstsInput -Name MultipleOutput -AsBool
-#[string]$TransformParameters   = Get-VstsInput -Name TransformParameters 
-#[string]$ArgumentsProviderPath = Get-VstsInput -Name ArgumentsProviderPath 
-#[string]$ArgumentsClass        = Get-VstsInput -Name ArgumentsClass 
+[string]$InputSourcePath       = Get-VstsInput -Name InputSourcePath
+[string]$TransformPath         = Get-VstsInput -Name TransformPath
+[string]$OutputDestinationPath = Get-VstsInput -Name OutputDestinationPath
+[string]$IncludePath           = Get-VstsInput -Name IncludePath  
+[string]$DocumentPath          = Get-VstsInput -Name DocumentPath
+[bool]$MultipleOutput          = Get-VstsInput -Name MultipleOutput -AsBool
+[string]$TransformParameters   = Get-VstsInput -Name TransformParameters 
+[string]$ArgumentsProviderPath = Get-VstsInput -Name ArgumentsProviderPath 
+[string]$ArgumentsClass        = Get-VstsInput -Name ArgumentsClass 
 
-[string]$InputSourcePath       = $args[0]
-[string]$TransformPath         = $args[1]
-[string]$OutputDestinationPath = $args[2]
-[string]$IncludePath           = $args[3]
-[string]$DocumentPath          = $args[4]
-[bool]$MultipleOutput          = $args[5]
-[string]$TransformParameters   = $args[6] 
-[string]$ArgumentsPath         = $args[7] 
-[string]$ArgumentsClass        = $args[8] 
+#[string]$InputSourcePath       = $args[0]
+#[string]$TransformPath         = $args[1]
+#[string]$OutputDestinationPath = $args[2]
+#[string]$IncludePath           = $args[3]
+#[string]$DocumentPath          = $args[4]
+#[bool]$MultipleOutput          = $args[5]
+#[string]$TransformParameters   = $args[6] 
+#[string]$ArgumentsPath         = $args[7] 
+#[string]$ArgumentsClass        = $args[8] 
 
 [string]$sMultipleOutput       = if($MultipleOutput) {'true'} else {'false'}
 [string]$ArgumentsProvider     = if($ArgumentsProviderPath -ne '') {$ArgumentsProviderPath + '::' + $ArgumentsClass} else {''} 
@@ -61,7 +62,6 @@ else
 {
     Write-Host "##vso[task.logissue type=error]Transform failed."
     Write-Output "##vso[task.complete result=Failed;]Finished"
-    exit 0
 }
 
 ## .\Transform.ps1 C:\Development\Projects\JTranOrg\JTran.CICD\Deployment\CustomTasks\JTranTransform\AzureDevOps\Test\blanksource.json C:\Development\Projects\JTranOrg\JTran.CICD\Deployment\CustomTasks\JTranTransform\AzureDevOps\Test\config.jtran C:\Development\Testing\JTran\JTranTransform\output.json C:\Development\Testing\JTran\JTranTransform C:\Development\Testing\JTran\JTranTransform false "-environment 'prod'" "C:\Development\Projects\JTranOrg\JTran\Tests\TestArgumentsProvider\bin\Debug\net8.0\TestArgumentsProvider.dll" TestArgumentsProvider.MyArgs
